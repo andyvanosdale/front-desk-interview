56523f4c87a4554b8fbdd0a246804f140fe74477
Andy Vanosdale <andy@vanosdale.com>
2015-02-16 23:38:13 -0800

    Initial Person model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150217073556) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "firstName"
+    t.string   "lastName"
+    t.date     "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
